61dadaaa8b30f3f8199c0e0dc20d54214b92e4f4
LENOVO <LENOVO@DESKTOP-LHNI7HA>
2023-09-29 17:03:30 +0100

    Revert try

--- src/main/java/com/example/membre/MembreApplication.java
+++ src/main/java/com/example/membre/MembreApplication.java
@@ -24,51 +24,7 @@ public abstract class MembreApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(MembreApplication.class, args);}
     public void run(String... args) throws Exception{
-        Etudiant etd1 = Etudiant.builder()
-                .cin("12345678")
-                .dateInscription(new Date())
-                .dateNaissance(new Date())
-                .diplome("mastère")
-                .email("etd1@gmail.com")
-                .password("pass1")
-                .cv("cv1")
-                .nom("abid")
-                .prenom("youssef)")
-                .sujet("dev")
-                .build();
-        /*
-        Etudiant etd2 = Etudiant.builder()
-                .cin("11141418")
-                .dateInscription(new Date())
-                .dateNaissance(new Date())
-                .diplome("mastère")
-                .email("etd2@gmail.com")
-                .password("pass2")
-                .cv("cv2")
-                .nom("boujelben")
-                .prenom("marwen)")
-                .sujet("blockhain")
-                .build();
-        EnseignantChercheur ens1 = EnseignantChercheur.builder()
-                .cin("22222222")
-                .dateNaissance(new Date())
-                .email("etd2@gmail.com")
-                .password("pass2")
-                .cv("cv2")
-                .nom("enseignant1")
-                .prenom("ens)")
-                .grade("enseignant")
-                .build();
-        EnseignantChercheur ens3 = EnseignantChercheur.builder()
-                .cin("33333333")
-                .dateNaissance(new Date())
-                .email("ens3@gmail.com")
-                .password("pass3")
-                .cv("cv3")
-                .nom("enseignant3")
-                .prenom("ens)")
-                .grade("enseignant")
-                .build();*/
+        
 
 
     }
